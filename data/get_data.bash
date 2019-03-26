2d8a6c66ce9fec21fc685d33d4b188888fabfa2c
sileod <damien.sileo@gmail.com>
2019-03-26 15:04:37 +0100

    Update get_data.bash

--- data/get_data.bash
+++ data/get_data.bash
@@ -1,1 +1,4 @@
-
+wget https://alex.irit.fr/depots/Discovery/DiscoveryBase.zip
+wget https://alex.irit.fr/depots/Discovery/DiscoveryBig.zip
+wget https://alex.irit.fr/depots/Discovery/DiscoveryBig.zip
+unzip *.zip
