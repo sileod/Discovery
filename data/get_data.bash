671d6d23e00c9aff973a1d1eb435f158df67bed3
sileod <damien.sileo@gmail.com>
2019-07-25 17:24:10 +0200

    Update get_data.bash

--- data/get_data.bash
+++ data/get_data.bash
@@ -14,5 +14,15 @@ wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='
 wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
      
+     
+export fileid=1ASQbmRu4LFfPYUD6gVYLyCQDvvqpraOH
+export filename=DiscoveryBig.zip
+
+## WGET ##
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+
+wget --load-cookies cookies.txt -O $filename \
+     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 
 unzip \*.zip
